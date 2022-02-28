7f6032dabd1a19aa9444b14223c3fac27a579b0a
Ramzin Nikita <rivorn73@gmail.com>
2022-02-28 19:49:38 +0500

    Initial commit

---
+++ EvolutionGame/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import com.company.controller.GameMenu;
+import com.company.view.GameVisual;
+import com.company.view.MenuVisual;
+
+public class Main {
+
+    public static void main(String[]args ){
+        GameMenu menu = new GameMenu();
+        MenuVisual.getGreeting();
+
+        menu.doCommandOfGame();
+        GameVisual.getFirstPlayer();
+        //  menu.setNamesOfPlayers();
+        //GamePhaseEvolution.b();
+
+    }
+}
